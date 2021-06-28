368728a7a0c39fc753c4f3b65ada8d4a33e7fe82
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-06-28 14:32:58 +0300

    API Yatube v1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -16,8 +16,10 @@ router_v1.register('follow', FollowViewSet, basename='follow')
 
 
 api_patterns_v1 = [
-    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('v1/', include(router_v1.urls)),
 ]
 
